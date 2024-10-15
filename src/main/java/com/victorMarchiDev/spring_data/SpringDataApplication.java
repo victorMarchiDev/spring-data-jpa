db38918081764655d2ee20504c507efde3e9b491
Victor Marchi <101611816+victorMarchiDev@users.noreply.github.com>
2024-10-15 16:06:37 -0300

    Testando primeiro commit

--- src/main/java/com/victorMarchiDev/spring_data/SpringDataApplication.java
+++ src/main/java/com/victorMarchiDev/spring_data/SpringDataApplication.java
@@ -2,10 +2,11 @@ package com.victorMarchiDev.spring_data;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class SpringDataApplication {
-
+	@GetMapping
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataApplication.class, args);
 	}
