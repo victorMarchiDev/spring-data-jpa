4aaeda8991170ece1d4b41779d6af77d665ecd35
Victor Marchi <101611816+victorMarchiDev@users.noreply.github.com>
2024-10-16 13:00:01 -0300

    Criando Classe User e Resource

--- src/main/java/com/victorMarchiDev/spring_data/SpringDataApplication.java
+++ src/main/java/com/victorMarchiDev/spring_data/SpringDataApplication.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class SpringDataApplication {
-	@GetMapping
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataApplication.class, args);
 	}
