c1c4b30ffc843fbbdb1e8016a9dfe18669736ec6
Victor Marchi <101611816+victorMarchiDev@users.noreply.github.com>
2024-10-18 09:35:56 -0300

    Implementando camada de serviço e registro de componentes

--- src/main/java/com/victorMarchiDev/spring_data/entities/User.java
+++ src/main/java/com/victorMarchiDev/spring_data/entities/User.java
@@ -41,6 +41,14 @@ public class User implements Serializable{
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
 
 	public String getEmail() {
 		return email;
---
+++ src/main/java/com/victorMarchiDev/spring_data/services/UserService.java
@@ -0,0 +1,26 @@
+package com.victorMarchiDev.spring_data.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.victorMarchiDev.spring_data.entities.User;
+import com.victorMarchiDev.spring_data.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
