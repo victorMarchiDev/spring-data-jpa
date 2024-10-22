015f68e35fe54d2dedd5b1d8801a5397601ce6e7
Victor Marchi <101611816+victorMarchiDev@users.noreply.github.com>
2024-10-22 11:15:58 -0300

    Criando o Enum OrderStatus

--- src/main/java/com/victorMarchiDev/spring_data/entities/Order.java
+++ src/main/java/com/victorMarchiDev/spring_data/entities/Order.java
@@ -29,13 +29,25 @@ public class Order implements Serializable{
 	@JoinColumn(name = "client_id")
 	private User client;
 
+	private Integer orderStatus;
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+	}
+
 	public Order() {
-		
 	}
 	
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
