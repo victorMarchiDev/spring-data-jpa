9388406e9b96fff0cd1b6d830a5a1dc6fbfbcb6d
Victor Marchi <101611816+victorMarchiDev@users.noreply.github.com>
2024-10-31 11:31:09 -0300

    Criando método de inserção de usuários

--- src/main/java/com/victorMarchiDev/spring_data/resources/UserResource.java
+++ src/main/java/com/victorMarchiDev/spring_data/resources/UserResource.java
@@ -1,16 +1,15 @@
 package com.victorMarchiDev.spring_data.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.victorMarchiDev.spring_data.entities.User;
 import com.victorMarchiDev.spring_data.services.UserService;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -29,6 +28,13 @@ public class UserResource {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
+
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+		.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
 	
 }
