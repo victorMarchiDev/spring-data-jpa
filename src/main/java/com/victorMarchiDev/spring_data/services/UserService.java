7b7122a9860941a68ee3c91f6549b7945facbddd
Victor Marchi <101611816+victorMarchiDev@users.noreply.github.com>
2024-11-01 16:25:53 -0300

    Tratando erros personalizados

--- src/main/java/com/victorMarchiDev/spring_data/services/UserService.java
+++ src/main/java/com/victorMarchiDev/spring_data/services/UserService.java
@@ -3,6 +3,7 @@ package com.victorMarchiDev.spring_data.services;
 import java.util.List;
 import java.util.Optional;
 
+import com.victorMarchiDev.spring_data.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj){
---
+++ src/main/java/com/victorMarchiDev/spring_data/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.victorMarchiDev.spring_data.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found! Id: " + id);
+    }
+
+
+}
